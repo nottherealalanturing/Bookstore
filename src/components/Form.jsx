b890932b87444d21ed9ba7ff63130550f98386c6
Assad Isah <nottherealalanturing@gmail.com>
2022-06-08 09:57:34 +0100

    feat: add dispatch for adding a book

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,44 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-const Form = () => (
-  <form className="book-form">
-    <input type="text" placeholder="Title" />
-    <input type="text" placeholder="Author" />
-    <button type="button" className="add-btn">
-      Add Book
-    </button>
-  </form>
-);
+const Form = (props) => {
+  const { handleUpdateProps, formStateProps, handleSubmitProps } = props;
+  return (
+    <form className="book-form" onSubmit={handleSubmitProps}>
+      <input
+        type="text"
+        placeholder="Title"
+        name="title"
+        onChange={handleUpdateProps}
+        value={formStateProps.title}
+      />
+      <input
+        type="text"
+        placeholder="Author"
+        name="author"
+        onChange={handleUpdateProps}
+        value={formStateProps.author}
+      />
+      <button type="submit" className="add-btn">
+        Add Book
+      </button>
+    </form>
+  );
+};
+
+Form.propTypes = {
+  handleUpdateProps: PropTypes.func,
+  formStateProps: PropTypes.shape({
+    title: PropTypes.string,
+    author: PropTypes.string,
+  }),
+  handleSubmitProps: PropTypes.func,
+};
+
+Form.defaultProps = {
+  handleUpdateProps: () => {},
+  formStateProps: {},
+  handleSubmitProps: () => {},
+};
 
 export default Form;
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from '../components/Book';
 import Form from '../components/Form';
+import { addBook } from '../redux/books/books';
 
 const Books = () => {
+  const [form, setForm] = React.useState({});
+
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  const handleUpdate = (event) => {
+    const { name, value } = event.target;
+    setForm((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (form.title && form.author) {
+      dispatch(addBook(form));
+    }
+    setForm({ title: '', author: '' });
+  };
+
   return (
     <div className="books">
       <ul>
         {books.map((book) => (
-          <Book author={book.author} title={book.title} key={book} />
+          <Book author={book.author} title={book.title} key={book.author} />
         ))}
       </ul>
-      <Form />
+      <Form
+        handleUpdateProps={handleUpdate}
+        formStateProps={form}
+        handleSubmitProps={handleSubmit}
+      />
     </div>
   );
 };

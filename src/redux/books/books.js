44eaca712ec67a30b114ebfdde1d72eecdfb2748
Assad Isah <nottherealalanturing@gmail.com>
2022-06-08 21:28:56 +0100

    fix: remove unused codes

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,61 +1,57 @@
 import axios from 'axios';
 
-const baseURL =
-  'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/PYvju9tS8O3lJv99pDDU/books';
+const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/PYvju9tS8O3lJv99pDDU/books';
 
 const ADD_BOOK = 'books/addBook';
 const DELETE_BOOK = 'books/remove';
 const GET_BOOKS = 'books/get';
 
-export const deleteBook = (id) => (dispatch) =>
-  axios({
-    method: 'delete',
-    url: `baseURL:${id}`,
+export const addBook = (book) => (dispatch) => axios({
+  method: 'post',
+  url: baseURL,
+  data: {
+    ...book,
+  },
+})
+  .then((res) => {
+    if (res.status === 201) {
+      dispatch({ type: ADD_BOOK, payload: book });
+    }
   })
-    .then((res) => {
-      if (res.status === 201) {
-        dispatch({ type: DELETE_BOOK, payload: id });
-      }
-    })
-    .catch(() => {});
+  .catch((e) => e);
 
-export const addBook = (book) => (dispatch) =>
-  axios({
-    method: 'post',
-    url: baseURL,
-    data: {
-      ...book,
-    },
+export const deleteBook = (id) => (dispatch) => axios({
+  method: 'delete',
+  url: `${baseURL}/${id}`,
+})
+  .then((res) => {
+    if (res.status === 201) {
+      dispatch({ type: DELETE_BOOK, payload: id });
+    }
   })
-    .then((res) => {
-      if (res.status === 201) {
-        dispatch({ type: ADD_BOOK, payload: book });
-      }
-    })
-    .catch(() => {});
+  .catch((e) => e);
 
-export const getBooks = () => (dispatch) =>
-  axios
-    .get(baseURL)
-    .then((res) => res.data)
-    .then((data) => {
-      const books = Object.keys(data).map((key) => {
-        const book = data[key][0];
-        return {
-          id: key,
-          ...book,
-        };
-      });
-      dispatch({ type: GET_BOOKS, payload: books });
-    })
-    .catch(() => {});
+export const getBooks = () => (dispatch) => axios
+  .get(baseURL)
+  .then((res) => res.data)
+  .then((data) => {
+    const books = Object.keys(data).map((key) => {
+      const book = data[key][0];
+      return {
+        id: key,
+        ...book,
+      };
+    });
+    dispatch({ type: GET_BOOKS, payload: books });
+  })
+  .catch((e) => e);
 
 const booksReducer = (books = [], action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...books, action.payload];
     case DELETE_BOOK: {
-      return books.filter((book) => book.title !== action.payload);
+      return books.filter((book) => book.id !== action.payload);
     }
     case GET_BOOKS: {
       return [...action.payload];

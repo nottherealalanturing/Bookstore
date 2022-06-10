afaeea7da893924e2fd0d8869715cb58a4335e88
Assad Isah <nottherealalanturing@gmail.com>
2022-06-10 21:25:34 +0100

    add logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CgProfile } from 'react-icons/cg';
 
 const Header = () => (
-  <>
+  <div className="navbar">
     <nav className="navigation">
       <h1 className="header-text">Bookstore CMS</h1>
       <Link to="/" className="link">
@@ -12,7 +13,8 @@ const Header = () => (
         CATEGORY
       </Link>
     </nav>
-  </>
+    <CgProfile className="profile" />
+  </div>
 );
 
 export default Header;
